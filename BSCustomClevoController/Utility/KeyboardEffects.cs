65507b1ed5580157b999ebf32c1014efe2861a98
Albert Frengki <albertfrengki@gmail.com>
2025-06-11 23:59:12 +0700

    Fix crash and Improvement Fix crash inside keyboard setting, english language, remove monitor mode causing issue on some device

--- BSCustomClevoController/Utility/KeyboardEffects.cs
+++ BSCustomClevoController/Utility/KeyboardEffects.cs
@@ -7,20 +7,17 @@ namespace BSCustomClevoController.Utility
     internal static class KeyboardEffects
     {
         //Set Effects
-
         public static KeyboardEffect Random = new KeyboardEffect() { Name = "Random", UniqId = 112, Id = 0 }; /// Goes every ~0.5 sec to a different random colour (with the current brightness)
-        public static KeyboardEffect Breath = new KeyboardEffect() { Name = "Nefes", UniqId = 16, Id = 2 }; /// Breaths with the current colour (cycles from 0 to brightness, around 2 sec)
-        public static KeyboardEffect Cycle = new KeyboardEffect() { Name = "Döngü", UniqId = 51, Id = 3 }; /// Cycles through primary and secondary colours (going from 0 to brightness in between, around 2sec). Blue, Green, Red, Blue?, Cyan, Yellow, Pink  
-        public static KeyboardEffect Wave = new KeyboardEffect() { Name = "Dalga", UniqId = 176, Id = 4 }; /// Slowely (around every 10 sec) shows a random colour slowly increasing and then decreasing the brightness (goes from 0 to brightness)
-        public static KeyboardEffect Dance = new KeyboardEffect() { Name = "Dans", UniqId = 128, Id = 5 }; /// Flashes every 0.5 sec with a different random colour (goes from 0 to brightness)
-        public static KeyboardEffect Tempo = new KeyboardEffect() { Name = "Tempo", UniqId = 144, Id = 6 }; /// Flashes a colour, then goes off shortly, then flashes the same colour, then goes off longer ~1sec, then starts over with a different colour (goes from 0 to brightness)
+        public static KeyboardEffect Breath = new KeyboardEffect() { Name = "Breath", UniqId = 16, Id = 2 }; /// Breathes with the current colour (cycles from 0 to brightness, around 2 sec)
+        public static KeyboardEffect Cycle = new KeyboardEffect() { Name = "Cycle", UniqId = 51, Id = 3 }; /// Cycles through primary and secondary colours (going from 0 to brightness in between, around 2 sec). Blue, Green, Red, Blue?, Cyan, Yellow, Pink  
+        public static KeyboardEffect Wave = new KeyboardEffect() { Name = "Wave", UniqId = 176, Id = 4 }; /// Slowly (around every 10 sec) shows a random colour slowly increasing and then decreasing the brightness (goes from 0 to brightness)
+        public static KeyboardEffect Dance = new KeyboardEffect() { Name = "Dance", UniqId = 128, Id = 5 }; /// Flashes every 0.5 sec with a different random colour (goes from 0 to brightness)
+        public static KeyboardEffect Tempo = new KeyboardEffect() { Name = "Tempo", UniqId = 144, Id = 6 }; /// Flashes a colour, then goes off shortly, then flashes the same colour, then goes off longer ~1 sec, then starts over with a different colour (goes from 0 to brightness)
         public static KeyboardEffect Flash = new KeyboardEffect() { Name = "Flash", UniqId = 160, Id = 7 }; /// Flashes every 1 sec with a different random colour (goes from 0 to brightness)
-        public static KeyboardEffect SoftwareBreath = new KeyboardEffect() { Name = "Nefes", UniqId = 0, Id = 8, Func = BreathEffect, Milisecons = 1 }; // , Milisecons=40
-        public static KeyboardEffect SoftwareCycle = new KeyboardEffect() { Name = "Döngü", UniqId = 0, Id = 9, Func = CycleEffect, Milisecons = 100 };
+        public static KeyboardEffect SoftwareBreath = new KeyboardEffect() { Name = "Breath", UniqId = 0, Id = 8, Func = BreathEffect, Milisecons = 1 }; // , Milisecons=40
+        public static KeyboardEffect SoftwareCycle = new KeyboardEffect() { Name = "Cycle", UniqId = 0, Id = 9, Func = CycleEffect, Milisecons = 100 };
 
         public static KeyboardEffect[] Effects = new KeyboardEffect[] { Random, Breath, Cycle, Wave, Dance, Tempo, Flash, SoftwareBreath, SoftwareCycle };
-
-
         static bool lowA = false;
         public static async void BreathEffect(object sender, EventArgs e)
         {
@@ -42,48 +39,41 @@ namespace BSCustomClevoController.Utility
         static Color targetColor = Color.Blue;
         public static Color Interpolate(this Color source, Color target)
         {
-            int r()
-            {
-                if (source.R > target.R) return source.R - 5;
-                else if (source.R < target.R) return source.R + 5;
-                else return source.R;
-            }
-            int g()
-            {
-                if (source.G > target.G) return source.G - 5;
-                else if (source.G < target.G) return source.G + 5;
-                else return source.G;
-            }
-            int b()
-            {
-                if (source.B > target.B) return source.B - 5;
-                else if (source.B < target.B) return source.B + 5;
-                else return source.B;
-            }
+            int clamp(int value) => Math.Max(0, Math.Min(255, value)); // Refactor
 
-            return Color.FromArgb(255, r(), g(), b());
+            int r = clamp(source.R + Math.Sign(target.R - source.R) * 5);
+            int g = clamp(source.G + Math.Sign(target.G - source.G) * 5);
+            int b = clamp(source.B + Math.Sign(target.B - source.B) * 5);
+
+            return Color.FromArgb(255, r, g, b);
         }
-        public static async void CycleEffect(object sender, EventArgs e)// Blue Green Red Cyan Yellow Pink
+
+        public static async void CycleEffect(object sender, EventArgs e) // Fix Crash
         {
             await System.Threading.Tasks.Task.Run(() =>
             {
-                KeyboardStatus status = KeyboardBackLight.GetStatus();
-
-                if (targetColor.ToArgb() != status.BackLight.ToArgb())
+                try
                 {
-                    Color newColor = Interpolate(status.BackLight, targetColor);
-                    KeyboardBackLight.SetColour(newColor.R, newColor.G, newColor.B);
-                }
-                else
-                {
-                    if (status.BackLight.ToArgb() == Color.Blue.ToArgb()) { targetColor = Color.FromArgb(255, 0, 125, 0); }
-                    else if (status.BackLight.ToArgb() == Color.FromArgb(255, 0, 125, 0).ToArgb()) targetColor = Color.Red;
-                    else if (status.BackLight.ToArgb() == Color.Red.ToArgb()) targetColor = Color.Cyan;
-                    else if (status.BackLight.ToArgb() == Color.Cyan.ToArgb()) targetColor = Color.Yellow;
-                    else if (status.BackLight.ToArgb() == Color.Yellow.ToArgb()) targetColor = Color.FromArgb(255, 255, 190, 205);
-                    else if (status.BackLight.ToArgb() == Color.FromArgb(255, 255, 190, 205).ToArgb()) targetColor = Color.Blue;
+                    KeyboardStatus status = KeyboardBackLight.GetStatus();
+                    Color current = status.BackLight; // Assuming BackLight is a Color struct
 
-                    KeyboardBackLight.SetColour(Interpolate(status.BackLight, targetColor));
+                    if (current.ToArgb() != targetColor.ToArgb())
+                    {
+                        Color newColor = Interpolate(current, targetColor);
+                        KeyboardBackLight.SetColour(newColor.R, newColor.G, newColor.B);
+                    }
+                    else
+                    {
+                        if (current.ToArgb() == Color.Blue.ToArgb()) targetColor = Color.FromArgb(255, 0, 125, 0); // Green
+                        else if (current.ToArgb() == Color.FromArgb(255, 0, 125, 0).ToArgb()) targetColor = Color.Red;
+                        else if (current.ToArgb() == Color.Red.ToArgb()) targetColor = Color.Cyan;
+                        else if (current.ToArgb() == Color.Cyan.ToArgb()) targetColor = Color.Yellow;
+                        else if (current.ToArgb() == Color.Yellow.ToArgb()) targetColor = Color.FromArgb(255, 255, 190, 205); // Pink
+                        else if (current.ToArgb() == Color.FromArgb(255, 255, 190, 205).ToArgb()) targetColor = Color.Blue;
+
+                        Color newColor = Interpolate(current, targetColor);
+                        KeyboardBackLight.SetColour(newColor.R, newColor.G, newColor.B);
+                    }
                 }
             });
         }
